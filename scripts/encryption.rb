0f2586dcf55c43b66d2af14102ed325668cbb1e7
Ugochukwu  Achara <Acharaachara2023@u.northwestern.edu>
2024-05-29 22:21:13 +0000

    almost done with auth class

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,11 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret ="1234"
+encrypted = BCrypt::Password.create(secret)
+puts encrypted
 # 2. prepare encrypted string for testing
+entered_password = "1234"
+outcome = BCrypt::Password.new(encrypted) == entered_password
 # 3. test secret against prepared encrypted string
+puts outcome
